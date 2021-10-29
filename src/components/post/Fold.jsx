louiskounios/blog

commit 734de39f1c7609069bbb94427c202e19a4b811e0

Fix broken links to blog posts
Fix issue in which the blog post list on any page but the 1st would
include broken links to blog posts.

Links to blog posts are now relative to the site URL rather than the
current URL so that pagination elements in the URL (e.g., /pages/n)
are not prepended to the blog post links.

src/components/post/Fold.jsx
@@ -65,14 +65,14 @@ class Fold extends React.Component {
 
   handleClick() {
     const { slug } = this.props;
-    navigate(slug);
+    navigate("/" + slug);
   }
 
   handleKeyPress(e) {
     const { slug } = this.props;
     const code = e.charCode || e.keyCode;
     if (code === 13 || code === 32) {
-      navigate(slug);
+      navigate("/" + slug);
     }
   }
 

commit 7f20d22436b03ed41ad4cb52dbcbbe9e98413e7e

Fix broken links to home
Bug in the navigation logic resulted in links to home relative to
the site URL (/) including an extra slash (//) which would break
routing. Fixed by conditionally navigating directly to home (/) where
appropriate.

Regression introduced by 734de39f1c7609069bbb94427c202e19a4b811e0.

src/components/post/Fold.jsx
@@ -65,14 +65,14 @@ class Fold extends React.Component {
 
   handleClick() {
     const { slug } = this.props;
-    navigate("/" + slug);
+    navigate(slug);
   }
 
   handleKeyPress(e) {
     const { slug } = this.props;
     const code = e.charCode || e.keyCode;
     if (code === 13 || code === 32) {
-      navigate("/" + slug);
+      navigate(slug);
     }
   }
 

src/components/post/Post.jsx
@@ -47,7 +47,7 @@ const Post = (props) => {
         date={date}
         interactive={summary !== undefined}
         mrDate={mrDate}
-        slug={slug}
+        slug={slug ? '/' + slug : '/'}
         title={title}
       />
 
@@ -56,7 +56,7 @@ const Post = (props) => {
       </Content>
       {html && <Comments />}
 
-      <Fold bottom interactive slug={slug || '/'}>
+      <Fold bottom interactive slug={slug ? '/' + slug : '/'}>
         {summary ? 'Read More' : 'Back Home'}
       </Fold>
     </PostContainer>